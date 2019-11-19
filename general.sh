b29ab5a6a1deedac6613b1ed895686117c1fcc4b
Valenci, Andrew <ansmeew@gmail.com>
2019-11-19 10:15:06 -0500

    Update general.sh

--- general.sh
+++ general.sh
@@ -9,6 +9,9 @@ sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
 sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
 sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
 
+sudo ip link set eth0 promisc on
+sudo ip link set eth1 promisc on
+
 sudo useradd -m -s /bin/bash seed
 sudo echo "seed:dees" | chpasswd
 sudo usermod -a -G sudo seed
